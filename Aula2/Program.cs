4f9ad47a33f9506c9ca8e8d5c1f03f37c4d97e40
Renato Luiz <renatoluiz9834@hotmail.com>
2023-06-02 12:10:57 -0300

    aula3

--- Aula2/Program.cs
+++ Aula2/Program.cs
@@ -16,3 +16,74 @@ if(nota>=5){
 }else{
     Console.WriteLine(REPROVADO);
 }
+
+/* switch(variavel)*/
+
+const string DIAS_31 = "ESTE MÊS TEM 31 DIAS!";
+const string DIAS_30 = "ESTE MÊS TEM 30 DIAS";
+const string DIAS_28 = "ESTE MÊS TEM 28 DIAS";
+const string MES_INVALIDO = "ESTE MÊS NÃO EXISTE";
+
+Console.WriteLine(("ESCREVA UM MÊS: "));
+string mes = Console.ReadLine();
+
+switch(mes){
+    case "JANEIRO":
+        Console.WriteLine(DIAS_31);
+        break;
+    case "FEVEREIRO":
+        Console.WriteLine(DIAS_28);
+        break;
+    case "MARÇO" :
+        Console.WriteLine(DIAS_31);
+        break;
+    case "ABRIL":
+        Console.WriteLine(DIAS_30);
+        break;
+     case "MAIO":
+        Console.WriteLine(DIAS_31);
+        break;
+    case "JUNHO":
+        Console.WriteLine(DIAS_30);
+        break;
+    case "JULHO" :
+        Console.WriteLine(DIAS_31);
+        break;
+    case "AGOSTO":
+        Console.WriteLine(DIAS_30);
+        break;
+    case "SETEMBRO":
+        Console.WriteLine(DIAS_30);
+        break;
+     case "OUTUBRO":
+        Console.WriteLine(DIAS_31);
+        break;
+    case "NOVEMBRO":
+        Console.WriteLine(DIAS_30);
+        break;
+    case "DEZEMBRO" :
+        Console.WriteLine(DIAS_31);
+        break;
+    default:
+    Console.WriteLine(MES_INVALIDO);
+    break;
+}
+
+/* WHILE AND FOR*/
+
+int[] arint = new int[] {1,2,3,4,5,6,7,8,9,10};
+int i = 0;
+while( i < arint.Length){
+    Console.WriteLine("Um array bem gay: " + arint[i]);
+    Console.WriteLine("Contando os i bem monete:" + i);
+    i++;
+}
+
+for( int j = 0; j <= 10; j++){
+    int numero = arint[j];
+    Console.WriteLine(numero);
+}
+
+foreach(int numero1 in arint){
+    Console.WriteLine(numero1);
+}
---
+++ Aula3/Program.cs
@@ -0,0 +1,94 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("---------------LISTAS----------------");
+int[] numeros = new int[] {1,2,3,4,5,6,7,8,9,10};
+int num = numeros[0];
+Console.WriteLine(num);
+
+int total = numeros.Length;
+
+List<int>listNumeros = new List<int>();
+
+List<int>listNum = new List<int>(){1,2,3,4,5};
+
+Console.WriteLine(listNum.Count);
+listNum.Add(6);
+listNum.Remove(2);
+
+foreach(int numero in listNum){
+    Console.WriteLine(numero);
+}
+
+Console.WriteLine("---------------Dicionario----------------");
+
+Dictionary<int, string> estados = new Dictionary<int, string>();
+estados.Add(1,"TO");
+estados.Add(2,"PA");
+estados.Add(3,"GO");
+
+if(!estados.ContainsKey(4)){
+    estados.Add(4,"MG");
+}
+
+foreach(string estado in estados.Values){
+    Console.WriteLine(estado);
+}
+
+string est = null;
+
+if(estados.TryGetValue(3, out est)){
+    Console.WriteLine(est);
+}
+
+
+Console.WriteLine("---------------QUEUE - FILA----------------");
+Queue<string> listaS = new Queue<string>();
+listaS.Enqueue("Ola");
+listaS.Enqueue("pessoal");
+listaS.Enqueue("feliz");
+listaS.Enqueue("mês");
+listaS.Enqueue("do Orgulho");
+
+foreach(string palavra in listaS){
+    Console.WriteLine(palavra);
+}
+Console.WriteLine("-------------DEQUEUE-----------------");
+string elemeto = listaS.Dequeue();
+foreach(string palavra in listaS){
+    Console.WriteLine(palavra);
+}
+int totalq = listaS.Count;
+Console.WriteLine("-------------Primeiro elemento e total-----------------");
+Console.WriteLine(listaS.Peek());
+Console.WriteLine(totalq);
+
+Console.WriteLine("-------------STACK - PILHA-----------------");
+
+Stack<string> listaP = new Stack<string>();
+
+listaP.Push("Amo");
+listaP.Push("meus");
+listaP.Push("gatos");
+listaP.Push("Chico");
+listaP.Push("Tigressa");
+
+foreach(string palavra in listaP){
+    Console.WriteLine(palavra);
+}
+Console.WriteLine("-------------POP-----------------");
+listaP.Pop();
+
+foreach(string palavra in listaP){
+    Console.WriteLine(palavra);
+}
+
+Console.WriteLine("-------------SORTEDLIST-----------------");
+
+SortedList<int,string> listaD = new SortedList<int, string>();
+listaD.Add(1, "Homen Aranha atravez do aranha verso");
+listaD.Add(4, "Homem Formiga Qunatumania");
+listaD.Add(2,"The Marvels");
+listaD.Add(3,"Guadioes da galaxia");
+
+foreach(string filme in listaD.Values){
+    Console.WriteLine(filme);
+}
